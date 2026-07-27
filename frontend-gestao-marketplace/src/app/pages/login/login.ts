5fa0d6f9f465a98fba86b6a5df95c8837d990ce7
joschonarth <joschonarth@gmail.com>
2026-07-27 20:18:58 -0300

    feat: implement reactive form for login with email and password validation

--- frontend-gestao-marketplace/src/app/pages/login/login.ts
+++ frontend-gestao-marketplace/src/app/pages/login/login.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
 export class Login {
-
+  userForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 }
