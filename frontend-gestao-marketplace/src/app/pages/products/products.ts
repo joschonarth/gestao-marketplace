37386ed65a69679bb26ca36b6b0fe769dd4125dd
joschonarth <joschonarth@gmail.com>
2026-07-28 08:16:51 -0300

    feat: implement product filtering functionality with reactive forms

--- frontend-gestao-marketplace/src/app/pages/products/products.ts
+++ frontend-gestao-marketplace/src/app/pages/products/products.ts
@@ -2,15 +2,22 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ProductsService } from '../../services/products';
 import { take } from 'rxjs';
 import { IProductResponse } from '../../interfaces/product-response';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
 export class Products implements OnInit {
   products: IProductResponse[] = [];
+  filteredProducts: IProductResponse[] = [];
+
+  filterForm = new FormGroup({
+    title: new FormControl(''),
+    status: new FormControl(''),
+  });
 
   private readonly _productsService = inject(ProductsService);
 
@@ -21,7 +28,19 @@ export class Products implements OnInit {
       .subscribe({
         next: (response) => {
           this.products = response.data;
+          this.filteredProducts = response.data;
         },
       });
+  }
+
+  filterProducts() {
+    const title = this.filterForm.value.title?.toLowerCase();
+    const status = this.filterForm.value.status?.toLowerCase();
+
+    this.filteredProducts = this.products.filter(
+      (product) =>
+        (!title || product.title.toLowerCase().includes(title)) &&
+        (!status || product.status.toLowerCase().includes(status)),
+    );
   }
 }
