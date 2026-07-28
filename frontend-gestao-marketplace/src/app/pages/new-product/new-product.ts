4e6033a297c20d55f68e2543d7f38ed7b00d1a0c
joschonarth <joschonarth@gmail.com>
2026-07-27 22:46:50 -0300

    feat: implement new product form with reactive form controls

--- frontend-gestao-marketplace/src/app/pages/new-product/new-product.ts
+++ frontend-gestao-marketplace/src/app/pages/new-product/new-product.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-product',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-product.html',
-  styleUrl: './new-product.css'
+  styleUrl: './new-product.css',
 })
 export class NewProduct {
+  productImageBase64 = '';
+  productForm = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    price: new FormControl(0, [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    category: new FormControl('', [Validators.required]),
+  });
 
+  saveProduct() {}
 }
